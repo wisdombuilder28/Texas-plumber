30f0821ce42c25e798b8bb7909e66697eee18151
wisdombuilder28 <adverbialagbawo@gmail.com>
2026-08-12 23:18:39 +0100

    update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -8,18 +8,20 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-storage.js";
 
+
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  // Copy this exact value from the console -- some older projects end in
-  // .appspot.com instead of .firebasestorage.app. Don't guess it.
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCZz-Nzke0GryitH-j8Lw6dplG6cbYxWdk",
+  authDomain: "n-d-flow-plumbing.firebaseapp.com",
+  projectId: "n-d-flow-plumbing",
+  storageBucket: "n-d-flow-plumbing.firebasestorage.app",
+  messagingSenderId: "641729795042"  ,
+  appId: "1:641729795042:web:d8228b55be8bc22e645fbb",
+  measurementId: "G-9LBTZMCXGV"
 };
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const storage = getStorage(app);  
